a26500b9883a965c18922c8e104962e8e6870ac1
Sameer Yadav <49569890+beagentcoder@users.noreply.github.com>
2021-08-24 12:42:58 +0530

    Add files via upload

---
+++ s.py
@@ -0,0 +1,35 @@
+#!/bin/bash
+import sys
+import socket
+from datetime import datetime 
+
+#define the target
+if len(sys.argv) ==2:
+	target=socket.gethostbyname(sys.argv[1]) #translate hostname to IPv4
+else:
+	print("Invalid Amount of arguments.")
+	print("Syntax:python3 scanner.py <ip>")
+
+#add a banner
+print("-" * 50)
+print("Time started:"+ str(datetime.now()))
+print("-" * 50)
+
+try:
+	for port in range(50,85):
+		s=socket.socker(socket.AF_INET,socket.SOCK_STREAM)
+		socket.setdefaulttimeout(1)
+		result=s.connect_ex((target,port)) #returns an error indicator
+		if result==0:
+			print("port {} is open ".format(port))
+		s.close()
+except KeyboardInterrupt:
+	print ("\nExiting program")
+	sys.exit()
+except socket.gaierror:
+	print("Hostname could not be resolved")
+	sys.exit()	
+except socket.error:
+	print("couldn't connect to server")
+	sys.exit()
+	
